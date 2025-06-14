093ab4e34beb9279af016306999468e20e28a042
Kenn-altsam <your_email@example.com>
2025-06-14 16:12:49 +0500

    update

--- back_2.2/src/celery_app.py
+++ back_2.2/src/celery_app.py
@@ -1,16 +1,16 @@
 from celery import Celery
 import os
 
-celery = Celery(
+celeryy = Celery(
     __name__,
     broker=os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0"),
     backend=os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0")
 )
 
-celery.autodiscover_tasks(['src.tasks'])
+celeryy.autodiscover_tasks(['src.tasks'])
 
 from celery.schedules import crontab
-celery.conf.beat_schedule = {
+celeryy.conf.beat_schedule = {
     "daily-fetch": {
         "task": "src.tasks.fetcher.fetch_daily_data",
         "schedule": crontab(hour=9, minute=0),
